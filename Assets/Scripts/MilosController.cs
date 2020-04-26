1e0a2f50b7eb482235a635171db3c87dccac848a
KameriiJ <jenny.sirarat@gmail.com>
2020-04-26 14:48:19 +0700

    fixed pc bugs

--- Assets/Scripts/MilosController.cs
+++ Assets/Scripts/MilosController.cs
@@ -7,14 +7,13 @@ public class MilosController : MonoBehaviour
 {
     public KeyCode crawl, handsUp;
     public Slider slider;
+    public Toggle toggle;
 
     Animator anim;
-    Vector3 fingerPos;
 
     private void Awake()
     {
         anim = transform.GetComponent<Animator>();
-        slider.value = 5;
     }
     
     void Start()
@@ -25,23 +24,48 @@ public class MilosController : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKeyDown(crawl) || slider.value <= 3)
+        if (toggle.isOn)
         {
-            anim.SetBool("handsUp", false);
-            anim.SetBool("crawl", true);
+            if (slider.value <= 3)
+            {
+                print("down");
+                anim.SetBool("handsUp", false);
+                anim.SetBool("crawl", true);
+            }
+            if (slider.value < 8 && slider.value > 3)
+            {
+                anim.SetBool("crawl", false);
+            }
+            if (slider.value >= 8)
+            {
+                anim.SetBool("crawl", false);
+                anim.SetBool("handsUp", true);
+            }
+            if (slider.value < 8 && slider.value > 3)
+            {
+                anim.SetBool("handsUp", false);
+            }
         }
-        if (Input.GetKeyUp(crawl) || (slider.value < 8 && slider.value > 3))
+        else
         {
-            anim.SetBool("crawl", false);
-        }
-        if (Input.GetKeyDown(handsUp) || slider.value >= 8)
-        {
-            anim.SetBool("crawl", false);
-            anim.SetBool("handsUp", true);
-        }
-        if (Input.GetKeyUp(handsUp) || (slider.value < 8 && slider.value > 3))
-        {
-            anim.SetBool("handsUp", false);
+            if (Input.GetKeyDown(crawl))
+            {
+                anim.SetBool("handsUp", false);
+                anim.SetBool("crawl", true);
+            }
+            if (Input.GetKeyUp(crawl))
+            {
+                anim.SetBool("crawl", false);
+            }
+            if (Input.GetKeyDown(handsUp))
+            {
+                anim.SetBool("crawl", false);
+                anim.SetBool("handsUp", true);
+            }
+            if (Input.GetKeyUp(handsUp))
+            {
+                anim.SetBool("handsUp", false);
+            }
         }
     }
 }
